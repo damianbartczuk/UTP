5c5166badd82f83dc197acdfb6fbb2bc46b55461
Damian <damian.bartczuk111@gmail.com>
2019-02-25 18:56:39 +0100

    ost

--- src/zad2/ProgLang.java
+++ src/zad2/ProgLang.java
@@ -9,7 +9,7 @@ import java.util.function.Predicate;
 import java.util.stream.Collectors;
 
 public class ProgLang {
-int wrt;
+
     private String path;
     private List<String> listOfProgrammers = new ArrayList<>();
     private List<String> content = new ArrayList<>();
@@ -17,6 +17,9 @@ int wrt;
     ProgLang(String path) {
         this.path = path;
         getContentFromFile(path);
+    }
+    public void show(){
+    System.out.println("moja metoda show");
     }
 
     private void getContentFromFile(String path) {
